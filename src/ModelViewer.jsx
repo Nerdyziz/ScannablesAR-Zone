3d336939909e05af88e5c2986e20a5c01ef56e99
Aziz_53 <avcreations53@gmail.com>
2025-12-04 23:14:58 +0530

    popup131

--- src/ModelViewer.jsx
+++ src/ModelViewer.jsx
@@ -146,7 +146,7 @@ export default function ModelViewer({ modelUrl, info }) {
 // === RESPONSIVE STYLE HELPER ===
 const boxStyle = (pos) => ({
   position: 'absolute',
-  width: '38%', 
+  width: '20%', 
   maxWidth: '160px',
   minHeight: '30px',
   padding: '8px',
